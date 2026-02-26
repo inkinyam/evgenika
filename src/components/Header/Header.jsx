9045e4270b020668fe377dede866040d94f64f70
Natalia Mezhakova <n.mezhakova@keep-calm.ru>
2026-02-26 10:38:47 +0300

    fix2: исправить ссылки в хедере

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,43 @@
-import './Header.scss';
-import iconInstagramm from '../../assets/icons/Icons-instagramm.svg';
-import iconTelegramm from '../../assets/icons/Icons-Telegram.svg';
-import iconVK from '../../assets/icons/Icons-VK.svg';
-import iconYoutube from '../../assets/icons/Icons-youtube.svg';
+import "./Header.scss";
+import iconInstagramm from "../../assets/icons/Icons-instagramm.svg";
+import iconTelegramm from "../../assets/icons/Icons-Telegram.svg";
+import iconVK from "../../assets/icons/Icons-VK.svg";
+import iconYoutube from "../../assets/icons/Icons-youtube.svg";
 
-import PropTypes from 'prop-types';
-const Header = ({social}) => {
+import PropTypes from "prop-types";
+const Header = ({ social }) => {
   return (
     <>
       <header className="header">
-        <p className='header__text'>Певица с уникальным голосом</p>
-        <div className='header__social'>
-          <a href={social.instagram} className="header__link">
-            <img src={iconInstagramm} alt="icon" />
-          </a>
-          <a href={social.youtube} className="header__link">
-            <img src={iconYoutube} alt="icon" />
-          </a>
-          <a href={social.telegram} className="header__link">
-            <img src={iconTelegramm} alt="icon" />
-          </a>
-          <a href={social.vk} className="header__link">
-            <img src={iconVK} alt="icon" />
-          </a>
-
+        <p className="header__text">Певица с уникальным голосом</p>
+        <div className="header__social">
+          {social.instagram && (
+            <a href={social.instagram} className="header__link">
+              <img src={iconInstagramm} alt="icon" />
+            </a>
+          )}
+          {social.youtube && (
+            <a href={social.youtube} className="header__link">
+              <img src={iconYoutube} alt="icon" />
+            </a>
+          )}
+          {social.telegram && (
+            <a href={social.telegram} className="header__link">
+              <img src={iconTelegramm} alt="icon" />
+            </a>
+          )}
+          {social.vk && (
+            <a href={social.vk} className="header__link">
+              <img src={iconVK} alt="icon" />
+            </a>
+          )}
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 Header.propTypes = {
-  social: PropTypes.object.isRequired
-} 
+  social: PropTypes.object.isRequired,
+};
 
 export default Header;
