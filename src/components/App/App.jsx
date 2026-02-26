d0c0bd9d6c706e923ddf1c159c0089ed1e64a2fd
Natalia Mezhakova <n.mezhakova@keep-calm.ru>
2026-02-26 10:34:25 +0300

    fix: доработать ссылки на соц.сети, добавить проверку на наличие ссылки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,19 @@
-import './App.scss';
-import Lead from '../Lead/Lead';
-import {Navigations, NavigationsItem} from '../Navigations/Navigations';
-import About from '../About/About';
-import Concerts from '../Concerts/Concerts';
-import Tracks from '../Tracks/Tracks';
-import News from '../News/News';
-import Videos from '../Videos/Videos';
-import Guides from '../Guides/Guides';
-import Footer from '../Footer/Footer';
-import Loader from '../Loader/Loader';
-import AppBackground from '../AppBackgound/AppBackground';
+import "./App.scss";
+import Lead from "../Lead/Lead";
+import { Navigations, NavigationsItem } from "../Navigations/Navigations";
+import About from "../About/About";
+import Concerts from "../Concerts/Concerts";
+import Tracks from "../Tracks/Tracks";
+import News from "../News/News";
+import Videos from "../Videos/Videos";
+import Guides from "../Guides/Guides";
+import Footer from "../Footer/Footer";
+import Loader from "../Loader/Loader";
+import AppBackground from "../AppBackgound/AppBackground";
 
-import React from 'react';
+import React from "react";
 
-import api from '../../api-utils/API';
-
-
+import api from "../../api-utils/API";
 
 function App() {
   const [publicationCards, setPublicationCards] = React.useState([]);
@@ -31,77 +29,94 @@ function App() {
 
   const [isLoading, setIsLoading] = React.useState(true);
 
-  React.useEffect(() => { 
-    Promise.all([  
-        api.getPublicationData(), 
-        api.getConcertsData(), 
-        api.getTracksData(), 
-        api.getVideosData(), 
-        api.getGuidesData(), 
-        api.getAboutData(), 
-        api.getContacstData(), 
-      ])
-           .then(([
-            publicationData, 
-            concertData,
-            tracksData,
-            videosData,
-            guidesData,
-            aboutData,
-            contactsData
-           ]) => {
-              setPublicationCards(publicationData); 
-              setConcertsCards(concertData); 
-              setTraksCards(tracksData); 
-              setVideosCards(videosData); 
-              setGuidesCards(guidesData); 
-              setAboutCards(aboutData.about);
-              setPromoCards(aboutData.promo);
-              setSocialCards(contactsData.social);
-              setConcertContactCards(contactsData['contact-block-1']);
-              setPRtContactCards(contactsData['contact-block-2']);
-            })
-           .catch((err) => console.log("ERROR: API return:" + err)) 
-           .finally(()=>{
-              setTimeout(() => {
-                setIsLoading(false);              
-              },2000); 
-            })      
-    }, [])
+  React.useEffect(() => {
+    Promise.all([
+      api.getPublicationData(),
+      api.getConcertsData(),
+      api.getTracksData(),
+      api.getVideosData(),
+      api.getGuidesData(),
+      api.getAboutData(),
+      api.getContacstData(),
+    ])
+      .then(
+        ([
+          publicationData,
+          concertData,
+          tracksData,
+          videosData,
+          guidesData,
+          aboutData,
+          contactsData,
+        ]) => {
+          setPublicationCards(publicationData);
+          setConcertsCards(concertData);
+          setTraksCards(tracksData);
+          setVideosCards(videosData);
+          setGuidesCards(guidesData);
+          setAboutCards(aboutData.about);
+          setPromoCards(aboutData.promo);
+          setSocialCards(contactsData.social);
+          setConcertContactCards(contactsData["contact-block-1"]);
+          setPRtContactCards(contactsData["contact-block-2"]);
+        },
+      )
+      .catch((err) => console.log("ERROR: API return:" + err))
+      .finally(() => {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      });
+  }, []);
 
-  return(
+  return (
     <>
-      <Loader isLoading={isLoading}/>
+      <Loader isLoading={isLoading} />
       <AppBackground isLoading={isLoading}>
-        <Lead data   = {promoCards}  
-              social = {socialCards}
-              concerts = {concertCards.length}/>
+        <Lead
+          data={promoCards}
+          social={socialCards}
+          concerts={concertCards.length}
+        />
         <main>
           <Navigations>
-            <NavigationsItem link='#about' text='о певице'/>  
-            { (concertCards.length > 0) && <NavigationsItem link='#concerts' text='Концерты'/> } 
-            { (trackCards.length > 0) &&  <NavigationsItem link='#tracks' text='треки'/>  }
-            { (publicationCards.length > 0) && <NavigationsItem link='#news' text='новости'/>  }
-            { (videoCards.length > 0) &&  <NavigationsItem link='#videos' text='клипы'/>  }
-            { (guidesCards.length > 0) &&<NavigationsItem link='#guides' text='продукция'/>  }
-            <NavigationsItem link='#contacts' text='контакты'/>  
-          </Navigations> 
+            <NavigationsItem link="#about" text="о певице" />
+            {concertCards.length > 0 && (
+              <NavigationsItem link="#concerts" text="Концерты" />
+            )}
+            {trackCards.length > 0 && (
+              <NavigationsItem link="#tracks" text="треки" />
+            )}
+            {publicationCards.length > 0 && (
+              <NavigationsItem link="#news" text="новости" />
+            )}
+            {videoCards.length > 0 && (
+              <NavigationsItem link="#videos" text="клипы" />
+            )}
+            {guidesCards.length > 0 && (
+              <NavigationsItem link="#guides" text="продукция" />
+            )}
+            <NavigationsItem link="#contacts" text="контакты" />
+          </Navigations>
 
-          <About data = {aboutCards}/> 
-          { (concertCards.length > 0) && <Concerts data = {concertCards}/> }
-          { (trackCards.length > 0) && <Tracks data = {trackCards}/> }
-          { (publicationCards.length > 0) && <News data = {publicationCards}/> }
-          { (videoCards.length > 0) && <Videos data = {videoCards} social = {socialCards}/> }
-          { (guidesCards.length > 0) && <Guides data = {guidesCards}/> }
+          <About data={aboutCards} />
+          {concertCards.length > 0 && <Concerts data={concertCards} />}
+          {trackCards.length > 0 && <Tracks data={trackCards} />}
+          {publicationCards.length > 0 && <News data={publicationCards} />}
+          {videoCards.length > 0 && (
+            <Videos data={videoCards} social={socialCards} />
+          )}
+          {guidesCards.length > 0 && <Guides data={guidesCards} />}
         </main>
-        
-        <Footer concertContact = {concertContactCards}
-                prContact      = {prContactCards}
-                social         = {socialCards}/>
 
+        <Footer
+          concertContact={concertContactCards}
+          prContact={prContactCards}
+          social={socialCards}
+        />
       </AppBackground>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,53 +1,67 @@
-import './Footer.scss';
-import iconInstagramm from '../../assets/icons/Icons-instagramm.svg';
-import iconTelegramm from '../../assets/icons/Icons-Telegram.svg';
-import iconVK from '../../assets/icons/Icons-VK.svg';
-import iconYoutube from '../../assets/icons/Icons-youtube.svg';
-import PropTypes from 'prop-types';
+import "./Footer.scss";
+import iconInstagramm from "../../assets/icons/Icons-instagramm.svg";
+import iconTelegramm from "../../assets/icons/Icons-Telegram.svg";
+import iconVK from "../../assets/icons/Icons-VK.svg";
+import iconYoutube from "../../assets/icons/Icons-youtube.svg";
+import PropTypes from "prop-types";
 
-const Footer = ({social, concertContact, prContact}) => {
-
+const Footer = ({ social, concertContact, prContact }) => {
   return (
-    <footer className='footer' id='contacts'>
+    <footer className="footer" id="contacts">
       <section className="footer__top">
         <div className="footer__column">
           <h4 className="footer__title">{concertContact.title}</h4>
           <p className="footer__text">{concertContact.name}</p>
-          <a href="tel:89995551212" className=" footer__text">{concertContact.phone}</a>
-          <a href="mailto:yandex@yandex.ru" className=" footer__text">{concertContact.email}</a>
+          <a href="tel:89995551212" className=" footer__text">
+            {concertContact.phone}
+          </a>
+          <a href="mailto:yandex@yandex.ru" className=" footer__text">
+            {concertContact.email}
+          </a>
         </div>
         <div className="footer__column">
           <h4 className="footer__title">{prContact.title}</h4>
           <p className="footer__text">{prContact.name}</p>
-          <a href="tel:89995551212" className="footer__text">{prContact.phone}</a>
-          <a href="mailto:yandex@yandex.ru" className="footer__text">{prContact.email}</a>
+          <a href="tel:89995551212" className="footer__text">
+            {prContact.phone}
+          </a>
+          <a href="mailto:yandex@yandex.ru" className="footer__text">
+            {prContact.email}
+          </a>
         </div>
       </section>
       <section className="footer__bottom">
         <h4 className="footer__title">Социальные сети певицы</h4>
 
         <div className="footer__social">
-        <a href={social.instagram} className="footer__link">
-            <img src={iconInstagramm} alt="icon" />
-          </a>
-          <a href={social.youtube} className="footer__link">
-            <img src={iconYoutube} alt="icon" />
-          </a>
-          <a href={social.telegram} className="footer__link">
-            <img src={iconTelegramm} alt="icon" />
-          </a>
-          <a href={social.vk} className="footer__link">
-            <img src={iconVK} alt="icon" />
-          </a>
+          {social.instagram && (
+            <a href={social.instagram} className="footer__link">
+              <img src={iconInstagramm} alt="icon" />
+            </a>
+          )}
+          {social.youtube && (
+            <a href={social.youtube} className="footer__link">
+              <img src={iconYoutube} alt="icon" />
+            </a>
+          )}
+          {social.telegram && (
+            <a href={social.telegram} className="footer__link">
+              <img src={iconTelegramm} alt="icon" />
+            </a>
+          )}
+          {social.vk && (
+            <a href={social.vk} className="footer__link">
+              <img src={iconVK} alt="icon" />
+            </a>
+          )}
         </div>
       </section>
-    
     </footer>
-  )
-}
+  );
+};
 Footer.propTypes = {
   social: PropTypes.object.isRequired,
   concertContact: PropTypes.object.isRequired,
   prContact: PropTypes.object.isRequired,
-}
+};
 export default Footer;
