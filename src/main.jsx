c99ec495d05c810305966541593ea002f86a70c7
Natalia Mezhakova <inkinyam@yandex.ru>
2026-05-13 11:34:27 +0300

    udpate:  Добавить дескрипшены, исправить устаревшие импорты, обновить зависимости

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'swiper/css';
+import 'swiper/css/bundle';
 import App from './components/App/App.jsx';
 import './index.scss';
 
